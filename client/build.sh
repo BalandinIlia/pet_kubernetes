95d705961636914500253a0e3ef18f695e1a2e12
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-11-17 11:21:03 +0100

    fix

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
 g++ -c runner.cpp
 g++ -c mainC.cpp
-g++ runner.o mainC.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:parameters.a -o client
+g++ runner.o mainC.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o client
 mv client ../executables
