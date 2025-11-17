abad82f1491559c0a2ce3338eeb8712b07aa6347
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-11-17 11:14:08 +0100

    fix

--- network_exec/build.sh
+++ network_exec/build.sh
@@ -1,4 +1,4 @@
 g++ -c clientInstance.cpp
 g++ -c mainS.cpp
-g++ clientInstance.o mainS.o -L../libraries -l:logger.a -l:networking_utils.a -l:networking_library.a -l:math_core.a -l:parameters.a -o network_exec
+g++ clientInstance.o mainS.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o network_exec
 mv network_exec ../executables
